a6cc8faa2589ec56b5bae39063af256b27b2804e
sadness_ojisan <sadness.ojisan@gmail.com>
2020-06-28 14:34:21 +0900

    fix tf

--- main.tf
+++ main.tf
@@ -9,17 +9,7 @@ resource "aws_s3_bucket" "main" {
 
   website {
     index_document = "index.html"
-    error_document = "error.html"
+    error_document = "index.html"
     routing_rules  = <<EOF
-[{
-    "Condition": {
-        "KeyPrefixEquals": "about/"
-    },
-    "Redirect": {
-        "ReplaceKeyPrefixWith": "about.html"
-    }
-}
-]
-EOF
   }
 }
