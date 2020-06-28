c1ba3708def91049408e043e8d71dc4bee846955
sadness_ojisan <sadness.ojisan@gmail.com>
2020-06-28 14:53:16 +0900

    add actions

--- main.tf
+++ main.tf
@@ -10,6 +10,5 @@ resource "aws_s3_bucket" "main" {
   website {
     index_document = "index.html"
     error_document = "index.html"
-    routing_rules  = <<EOF
   }
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function () {
   return (
     <div>
-      This is a Root Page. aboutページは<Link href="about/">こちら</Link>
+      This is a Root Page. aboutページは<Link href="about/">こちら〜</Link>
       から！！
     </div>
   );
