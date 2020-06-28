4d113aeae010b643d92994f3747a9a41187585cc
sadness_ojisan <sadness.ojisan@gmail.com>
2020-06-28 16:25:43 +0900

    resolve policy

--- main.tf
+++ main.tf
@@ -5,10 +5,31 @@ provider "aws" {
 
 resource "aws_s3_bucket" "main" {
   bucket = "s3-nextjs-deploy"
-  acl    = "private"
+  acl    = "public-read"
 
   website {
     index_document = "index.html"
     error_document = "index.html"
   }
 }
+
+resource "aws_s3_bucket_policy" "main" {
+  bucket = aws_s3_bucket.main.id
+  policy = data.aws_iam_policy_document.bucket_policy_document.json
+}
+
+data "aws_iam_policy_document" "bucket_policy_document" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    resources = [
+      "${aws_s3_bucket.main.arn}",
+      "${aws_s3_bucket.main.arn}/*"
+    ]
+    actions = ["s3:GetObject"]
+  }
+}
+
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,6 +4,7 @@ export default function () {
       This is a About Page!! <br></br>
       みてみて〜画像ファイルのパスも解決できてるよ〜〜〜〜〜<br></br>
       <img src="/image.png"></img>
+      やったね！
     </div>
   );
 }
